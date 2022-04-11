74c15467015de175fef1ae738442c45079ce4529
longknives <76183876+longknives@users.noreply.github.com>
2022-04-11 02:17:24 -0400

    Create UI.py

---
+++ UI.py
@@ -0,0 +1,9 @@
+import colorama
+from colorama import Fore
+
+def printRat():
+ print(Fore.RED+"          ___")
+ print(" _  _  .-'   '-.")
+ print("(.)(.)/         \   Rat Analysis Stock Market")
+ print(" /@@             ;")
+ print("o_\\-mm-......-mm`~~~~~~~~~~~~~~~~`")
